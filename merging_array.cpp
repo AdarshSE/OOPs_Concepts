99510fada48ec91d8e8e8307b9a7f1809b073c39
AdarshSE <adarsh.25b01010519@abes.ac.in>
2026-08-31 11:33:01 +0530

    OOPS Concept

--- merging_array.cpp
+++ merging_array.cpp
@@ -2,14 +2,14 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int arr[5] = {1, 2, 3, 4, 5};
-    int arr1[5] = {6, 7, 8, 9, 10};
+    int arr[6] = {1, 2, 3, 4, 5, 6};
+    int arr1[4] = {7, 8, 9, 10};
     int arr2[10];
-    for(int i = 0; i < 5; i++) {
+    for(int i = 0; i < 6; i++) {
         arr2[i] = arr[i];
     }
-    for(int i = 0; i < 5; i++) {
-        arr2[i + 5] = arr1[i];
+    for(int i = 0; i < 4; i++) {
+        arr2[i + 6] = arr1[i];
     }
     for(int i = 0; i < 10; i++) {
         cout << arr2[i] << " ";
