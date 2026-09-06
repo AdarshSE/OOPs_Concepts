770b01f31ddeb437bc38da5f2abe3c94f1e84e4c
AdarshSE <adarsh.25b01010519@abes.ac.in>
2026-09-06 15:29:03 +0530

    OOPs Concepts

---
+++ Scoperesolution_autokeyword.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+auto num = 10;
+class Test {
+    public:
+    void greetHello();
+    void greetHi();
+    void greetHalo();
+};
+void Test::greetHello() {
+    cout << "Hello!" << endl;
+}   
+void Test::greetHi() {
+    cout << "Hi!" << endl;
+}
+void Test::greetHalo() {
+    cout << "Halo!" << endl;
+}
+int main() {
+    Test t;
+    int num;
+    cin>>num;
+    if(::num > num)
+       t.greetHello();
+    else if(::num == num)
+        t.greetHi();
+    else 
+        t.greetHalo();
+    return 0;
+}
